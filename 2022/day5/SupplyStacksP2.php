791abad780ff7bbca37b761eefedbcc9943ac9a5
Felix Jonas Wiegleb <js-wiegleb@t-online.de>
2022-12-13 11:12:22 +0100

    Solve day5 part two

---
+++ 2022/day5/SupplyStacksP2.php
@@ -0,0 +1,39 @@
+<?php
+
+require_once 'Misc.php';
+
+$filename = "input.txt";
+if (!file_exists($filename)) {
+    echo "Input file not found!" . PHP_EOL;
+    exit;
+}
+
+$file = fopen($filename, "rb");
+
+$crates = readCrateInput($file);
+$tmp = [];
+
+while ($line = fgets($file)) {
+    if (strpos($line, 'move') === false) {
+        continue;
+    }
+
+    $matches = explode(' ', $line);
+    $amount = (int)$matches[1];
+    $from = ((int)$matches[3]) - 1;
+    $to = ((int)$matches[5]) - 1;
+
+    for ($i = 0; $i < $amount; $i++) {
+        $tmp[] = array_pop($crates[$from]);
+    }
+    for ($i = 0; $i < $amount; $i++) {
+        $crates[$to][] = array_pop($tmp);
+    }
+}
+fclose($file);
+
+$solution = "";
+foreach ($crates as $crate) {
+    $solution .= end($crate);
+}
+echo PHP_EOL . $solution . PHP_EOL;
